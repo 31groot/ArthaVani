6683f57ebb8372ba821ff44aec6e263693272f95
Parv Agrawal <agrawalparv13@gmail.com>
2026-08-10 23:06:56 +0530

    added the SPEECH_THRESHOLD

--- config/constants.py
+++ config/constants.py
@@ -10,7 +10,9 @@ CHUNK_SIZE = 1024
 # 16-bit PCM audio
 SAMPLE_WIDTH = 2
 
-
+# Silero VAD frame size (samples)
+# 512 samples at 16 kHz = 32 ms
+VAD_FRAME_SAMPLES = 512
 
 # Minimum speech duration (milliseconds)
 MIN_SPEECH_DURATION_MS = 250
@@ -18,13 +20,15 @@ MIN_SPEECH_DURATION_MS = 250
 # Minimum silence duration before considering speech ended (milliseconds)
 MIN_SILENCE_DURATION_MS = 500
 
+# Threshold for speech detection
+SPEECH_THRESHOLD = 0.5
+
 
 STT_TIMEOUT = 30
 
 LLM_TIMEOUT = 60
 
 TTS_TIMEOUT = 30
-
 
 
 
