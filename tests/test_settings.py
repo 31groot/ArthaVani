5239391e7f0c663457d81dd8b6a560faad02e6ae
Parv Agrawal <agrawalparv13@gmail.com>
2026-09-23 07:05:55 +0530

    test: remove obsolete settings smoke test

--- tests/test_settings.py
+++
@@ -1,6 +0,0 @@
-from config.settings import settings
-
-print("Azure Deployment :", settings.azure_openai_deployment)
-print("Deepgram Model   :", settings.deepgram_model)
-print("Voice ID         :", settings.elevenlabs_voice_id)
-print("Log Level        :", settings.log_level)
